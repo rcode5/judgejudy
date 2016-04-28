ee83a502ea650a2efc0bd7f5db9f352b04a26da9
Mr Rogers <jon@carbonfive.com>
2016-04-28 12:41:17 -0700

    chuck

--- spec/commands/rules_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Pardner::Commands::Rules, type: :bot do
-  it 'responds with the rules of the game' do
-    expect(message: 'rules of the west').to respond_with_slack_message(/Projects will be judged based on/)
-  end
-end
